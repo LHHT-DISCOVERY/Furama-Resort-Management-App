e2b2f721fea16483d11c74196f8bcec77d1e2990
LHHT-DISCOVERY <lytri102@gmail.com>
2023-09-10 23:42:13 +0700

    .

--- src/main/java/com/example/furamaresortmanagementapp/service/IAccountService.java
+++ src/main/java/com/example/furamaresortmanagementapp/service/IAccountService.java
@@ -3,5 +3,5 @@ package com.example.furamaresortmanagementapp.service;
 import com.example.furamaresortmanagementapp.model.employee.Account;
 
 public interface IAccountService {
-    Account findAllByUsername(String username) ;
+    Account findAllByUsername(String username);
 }
